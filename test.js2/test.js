de8a8b0895c7a6b7695f22f1d197028261ceec98
ignatiusngeyen4 <ignatiusngeyen4@gmail.com>
2025-12-16 14:44:43 +0100

    added html and css

---
+++ test.js2/test.js
@@ -0,0 +1,21 @@
+let UserName = "IgnatiusOscar"
+let age = 30
+
+console.log(UserName)
+console.log(" my name is " + UserName + "  and I am" + age)
+
+Function(add ( num1, num2)) {
+  let sum = num1 + num 2 
+     return sum;
+}
+  console.log (6,7,)
+
+  if( age >= 80) {
+    console.log(" you are an old fool.")
+  } else if( age >= 24){
+    console.log(" you are an adult.")
+  }else {
+    console.log(" you are a child.")
+    
+  }
+ 
